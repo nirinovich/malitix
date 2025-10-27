d4e374b1e210e4f62fa0e227d621a8e7f57aa0f1
nirinovich <nikitanirinovich@gmail.com>
2025-10-27 13:36:48 +0300

    added scroll top when switching pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from './context/ThemeContext';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import LegalNotice from './pages/LegalNotice';
@@ -8,15 +8,13 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/mentions-legales" element={<LegalNotice />} />
-            <Route path="/politique-de-confidentialite" element={<PrivacyPolicy />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mentions-legales" element={<LegalNotice />} />
+          <Route path="/politique-de-confidentialite" element={<PrivacyPolicy />} />
+        </Routes>
+      </Layout>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from "react-router";
+import ScrollToTop from './components/ScrollToTop.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
