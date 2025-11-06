1c287b588a0c9a0492f4af5e8f9d246bc86d97b5
nirinovich <nikitanirinovich@gmail.com>
2025-11-06 13:53:12 +0300

    updated contact number

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -66,7 +66,7 @@ export const COMPANY_INFO = {
   name: 'Malitix',
   tagline: 'Excellence technologique, innovation continue',
   email: 'contact@malitix.com',
-  phone: 'To add',
+  phone: '+261 34 49 796 50',
   address: 'Antananarivo, Madagascar',
   description: 'Malitix est votre partenaire de confiance pour la transformation digitale et l\'innovation technologique.',
 };
