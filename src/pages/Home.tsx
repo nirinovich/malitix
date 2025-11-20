1ad3764e168ff56737845247a0458735fcf59607
Nirinovich <nikitanirinovich@gmail.com>
2025-11-20 16:09:06 +0300

    removed react helmet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet-async';
 import Hero from '../components/Home/Hero';
 import ServicesBento from '../components/Home/ServicesBento';
 import TrustStats from '../components/Home/TrustStats';
@@ -7,25 +6,6 @@ import CTASection from '../components/Home/CTASection';
 export default function Home() {
   return (
     <>
-      <Helmet>
-        <title>Malitix | Excellence technologique, innovation continue</title>
-        <meta name="description" content="Malitix accompagne les entreprises dans leur transformation digitale avec expertise technique, innovation et excellence opérationnelle. Solutions sur-mesure pour vos projets IT." />
-        <meta name="keywords" content="transformation digitale, expertise technique, développement web, consulting IT, Malitix" />
-        <link rel="canonical" href="https://www.malitix.com/" />
-        
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.malitix.com/" />
-        <meta property="og:title" content="Malitix | Excellence technologique, innovation continue" />
-        <meta property="og:description" content="Malitix accompagne les entreprises dans leur transformation digitale avec expertise technique, innovation et excellence opérationnelle." />
-
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.malitix.com/" />
-        <meta property="twitter:title" content="Malitix | Excellence technologique, innovation continue" />
-        <meta property="twitter:description" content="Malitix accompagne les entreprises dans leur transformation digitale avec expertise technique, innovation et excellence opérationnelle." />
-      </Helmet>
-
       {/* Hero Section */}
       <Hero />
       
--- src/pages/SprintCommando.tsx
+++ src/pages/SprintCommando.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet-async';
 import HeroVariantC from '../components/Sprint/HeroVariantC';
 import SprintProblem from '../components/Sprint/SprintProblem';
 import SprintSolution from '../components/Sprint/SprintSolution';
@@ -9,25 +8,6 @@ import { BenefitsShowcaseVariantB } from '../components/Sprint/BenefitsShowcaseV
 export default function SprintCommando() {
   return (
     <>
-      <Helmet>
-        <title>Sprint Commando - Déblocage garanti en 14 jours | Malitix</title>
-        <meta name="description" content="Notre Sprint Commando débloque vos projets tech en 14 jours maximum. Intervention rapide, expertise technique pointue, résultats garantis. Contactez-nous." />
-        <meta name="keywords" content="sprint commando, déblocage projet, intervention rapide, expertise technique, Malitix" />
-        <link rel="canonical" href="https://www.malitix.com/sprint-commando" />
-        
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.malitix.com/sprint-commando" />
-        <meta property="og:title" content="Sprint Commando - Déblocage garanti en 14 jours | Malitix" />
-        <meta property="og:description" content="Notre Sprint Commando débloque vos projets tech en 14 jours maximum. Intervention rapide, expertise technique pointue, résultats garantis." />
-
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.malitix.com/sprint-commando" />
-        <meta property="twitter:title" content="Sprint Commando - Déblocage garanti en 14 jours | Malitix" />
-        <meta property="twitter:description" content="Notre Sprint Commando débloque vos projets tech en 14 jours maximum. Intervention rapide, expertise technique pointue, résultats garantis." />
-      </Helmet>
-
       {/* Hero Section */}
       <HeroVariantC />
 
