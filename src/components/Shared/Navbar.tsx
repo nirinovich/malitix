da6db2eb01f2dd31ee0934d08b8701734111e76c
Nirinovich <nikitanirinovich@gmail.com>
2025-12-08 11:44:26 +0300

    added to navbar

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -11,6 +11,7 @@ interface NavbarProps {
 const USE_CASES = [
   { label: 'Sprint Commando', href: '/sprint-commando', description: 'Déblocage garanti en 14 jours' },
   { label: 'Développement Sur Mesure', href: '/developpement-sur-mesure', description: 'Application web & mobile en 90 jours' },
+  { label: 'Refonte SI', href: '/refonte-si', description: 'Modernisation de système d\'information' },
   // Add more use cases here in the future
 ];
 
